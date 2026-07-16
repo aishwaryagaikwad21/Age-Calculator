6564ac9778257a18fc2ed771878db3877a4a8266
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-07-16 20:31:55 +0530

    age calculator structure

--- ageCalculator.js
+++ ageCalculator.js
@@ -3,6 +3,35 @@ const currentYear = date.getFullYear();
 const currentMonth = date.getMonth() + 1;
 const currentDate = date.getDate();
 
+const dobInput = document.getElementById("dob");
+const calculateBtn = document.getElementById("calculate-btn")
+const result = document.getElementById("result")
+
+calculateBtn.addEventListener("click", () => {
+    const dobValue = dobInput.value;
+    //console.log(dobValue)
+    const parts = dobValue.split("-")
+    //console.log(parts);
+    const birthYear = Number(parts[0])
+    const birthMonth = Number(parts[1]);
+    const birthDate = Number(parts[2]);
+    const age = ageCalculator(birthDate, birthMonth, birthYear);
+    //console.log(age)
+    if(age.birthday){
+        result.innerText = `Happy Birthday 🎉! you are ${age.calcYear} years old`
+    }
+    else{
+        result.innerText = `You are ${age.calcYear} years ${age.calcMonths} months and ${age.calcDays} days old`
+    }
+})
+
+//reset
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", () => {
+    dobInput.value = "";
+    result.innerText = "";
+});
+
 function ageCalculator(birthDate, birthMonth, birthYear){ //main function to take value from form/UI and update the DOM
     if(birthYear > currentYear || birthDate > 31 || birthDate < 1 || birthMonth > 12 || birthMonth < 1){ //return error
         return console.error('Invalid date!');
@@ -14,12 +43,24 @@ function ageCalculator(birthDate, birthMonth, birthYear){ //main function to tak
     let calcDays = dayCalculation(birthDate)
 
     if(calcMonths === 0 && calcDays === 0){
-        console.log(`Happy Birthday🎉!, you're ${calcYear} years old.`)
+        //console.log(`Happy Birthday🎉!, you're ${calcYear} years old.`)
+        return {
+            calcYear,
+            calcMonths:0,
+            calcDays:0,
+            birthday: true
+        }
     }
     else{
-        console.log(calcYear)
+        /* console.log(calcYear)
         console.log(calcMonths)
-        console.log(calcDays)
+        console.log(calcDays) */
+        return {
+            calcYear,
+            calcMonths,
+            calcDays,
+            birthday: false
+        }
     }
 }
 
